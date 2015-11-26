6df5579bb908f3e04a8f41e20c246e9422d2a1f9
Kunal Lanjewar <kunallanjewar@gmail.com>
2015-11-26 08:57:55 -0800

    finished code

---
+++ ex01.py
@@ -0,0 +1,9 @@
+#Student : Kunal Lanjewar
+#Programming Excercise: 01
+
+import MessageBox
+MyName = "Kunal Lanjewar"
+Learning = MyName + " is learning Python!"
+
+  
+MessageBox.Show(Learning) 
---
+++ ex02.py
@@ -0,0 +1,11 @@
+#Student: Kunal Lanjewar
+#Programming Exercise: 02
+
+ans = [] #empty list
+
+weight = input('How much do you weigh in lbs?: ')  
+ans.append(weight)
+
+# 1 lbs = 0.453592 kg
+
+print('You weight 78.4 ',(float(weight)*0.453592), "kg.")
